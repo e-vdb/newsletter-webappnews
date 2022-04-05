1bdb1f07320ee824582de0c90d3b0580c9be76b3
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-04-05 10:48:49 +0200

    Add directory creation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from newsletter.newsletter import Newsletter
+import os
+from pathlib import Path
+from os.path import dirname, join
 
 
 def main():
@@ -10,4 +13,8 @@ def main():
 
 
 if __name__ == '__main__':
+    try:
+        os.makedirs(join(Path(dirname(__file__)), 'wordcloud_fig'))
+    except OSError as e:
+        print('folder already exists')
     main()
